600824e4f9f19408e285b7390d285d88ea1aef8b
arnoldoJose <aolivasgonzales73@gmail.com>
2020-12-27 16:47:08 -0600

    added route public

--- Server.js
+++ Server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const key = require('./Config/key');
 const app = express();
 const fs = require('fs');
+const { execPath } = require('process');
 
 require('./Config/config');
 
@@ -29,7 +30,8 @@ app.use(session({
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
 app.use(require('./Routes/rutas'))
+
+app.use("/public", express.static(`${__dirname}/storage`));
 
 app.listen(process.env.PORT ,() => console.log('conectado al servidor ' + process.env.PORT));
